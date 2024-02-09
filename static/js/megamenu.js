ab316c17fd419428762282852b8ea30f815e2772
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2024-02-09 11:38:22 -0300

    Minor fixes in mega menu

--- static/js/megamenu.js
+++ static/js/megamenu.js
@@ -7,10 +7,31 @@ $('.dropdown-item').click(function() {
     $(this).addClass('active'); // add onto current
 });
 */
-$('.dropdown-item').hover(function() {
-    $('a.dropdown-item').removeClass('active'); 
-    $('div.megamenu-item-tab').removeClass('active'); 
-    $(this).addClass('active');
-    var elem = document.getElementById('item-'.concat($(this)[0].id));
-    $(elem).addClass('active');
+
+$('.megamenu-li').hover(function() {
+  var elems = $(this)[0].querySelector('.megamenu-element').getElementsByClassName('active');
+  if (elems.length == 0) {
+    $($(this)[0].querySelector('a.dropdown-item')).addClass('active');
+    $($(this)[0].querySelector('div.megamenu-item-tab')).addClass('active');
+  }
 });
+
+$('.dropdown-item').hover(function() {
+  $('a.dropdown-item').removeClass('active'); 
+  $('div.megamenu-item-tab').removeClass('active'); 
+  $(this).addClass('active');
+  $(document.getElementById('item-'.concat($(this)[0].id))).addClass('active');
+});
+
+$('.megamenu-desktop').hover(function() {
+  var item = document.querySelector('div.dropdown-item#'.concat($(this)[0].id));
+  var elems = $(item)[0].getElementsByClassName('active');
+  if (elems.length == 0) {
+    var newItem = $(item)[0].querySelector('a.dropdown-item');
+    $(newItem).addClass('active');
+    $(document.getElementById('item-'.concat($(newItem)[0].id))).addClass('active');
+  }
+});
+
+
+
